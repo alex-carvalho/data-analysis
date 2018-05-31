6acc518d859918842d9698f504d05910c4719404
Alex Carvalho <alex_carvalho@outlook.com.br>
2018-05-31 08:54:48 -0300

    Add spring boot and spring starter quartz

---
+++ src/main/java/br/com/ac/DataAnalysisApplication.java
@@ -0,0 +1,15 @@
+package br.com.ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Alex Carvalho
+ */
+@SpringBootApplication
+public class DataAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAnalysisApplication.class, args);
+    }
+}
